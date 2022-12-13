64961bf7c577bf44d9908292941ca3523ec41809
davedev <davidabolade29@gmail.com>
2022-12-13 15:33:55 +0100

    halfway through

---
+++ src/components/Home/Logo/index.js
@@ -0,0 +1,13 @@
+import './index.scss';
+import logoS from "../../../assets/images/logo-s.png"
+
+const Logo = () => {
+    return (
+        
+            <div className="logo-container">
+                <img src={logoS} alt="" className="solid-logo" />
+            </div>
+        )
+}
+
+export default Logo
